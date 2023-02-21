2d0392762219d9efe49d06f1c3eefa2f932780f4
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-02-21 16:46:11 +0100

    add score and logic to parsing

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -12,8 +12,17 @@ class WordsController < ApplicationController
   def score
     @word = params[:word]
     @grid = params[:grid]
+    @score = 0
     @url = "https://wagon-dictionary.herokuapp.com/#{@word}"
     @user_serialized = URI.open(@url).read
     @user = JSON.parse(@user_serialized)
+    if @user["found"] == false
+      @answer = "You get 0 points! It is no an english word"
+    # elsif @word.split("").include_all?(@grid) == false
+    #   @answer = "Word is not matching the grid"
+    else
+      @answer = "Good job. You are getting #{@word.length} points"
     end
+    @answer
   end
+end
